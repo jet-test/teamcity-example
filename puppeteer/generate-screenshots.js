36e935aa5a5a90bb6d1429c25c43448fc430170c
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-07-22 12:22:24 +0200

    Updated puppeteer launch

--- puppeteer/generate-screenshots.js
+++ puppeteer/generate-screenshots.js
@@ -6,7 +6,10 @@ const {buffer, diffDir} = require('./paths');
     await cleanDirectory(buffer)
     await cleanDirectory(diffDir)
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage']
+    });
     const page = await browser.newPage();
     await page.goto('http://localhost:8041');
     await page.screenshot({path: `buffer/overview.png`});
