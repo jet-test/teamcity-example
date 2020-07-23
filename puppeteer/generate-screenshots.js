1dbab78db814534e93c22234429f05bd2dd23706
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2020-07-23 16:38:14 +0700

    remove browser args

--- puppeteer/generate-screenshots.js
+++ puppeteer/generate-screenshots.js
@@ -6,10 +6,7 @@ const {buffer, diffDir} = require('./paths');
     await cleanDirectory(buffer)
     await cleanDirectory(diffDir)
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage', "--no-sandbox", "--disable-setuid-sandbox"]
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://frontend:8041');
     await page.screenshot({path: `buffer/overview.png`});
