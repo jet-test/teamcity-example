d8d00e618e8d1bdb24aa883c407d409928754e1b
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2020-07-23 16:59:46 +0700

    add TC messages

--- puppeteer/compare.js
+++ puppeteer/compare.js
@@ -5,11 +5,11 @@ const pixelmatch = require('pixelmatch');
 const {buffer, diffDir, stable} = require('./paths');
 const {readBufferedFiles} = require("./utils")
 
-async function compareImages(filename) {
+async function compareImages(filename, flowId) {
     let generatedFile = null
     let originalFile = null
-    let testName = escapeTCMessage("compare-" + filename);
-    console.log(`##teamcity[testStarted name='${testName}']`)
+    let testName = escapeTCMessage(`compare-${filename.replace(/\./g, "_")}`);
+    console.log(`##teamcity[testStarted name='${testName}' flowId='${flowId}']`)
     let startTime = Date.now()
     try {
         generatedFile = await fs.promises.readFile(`${buffer}/${filename}`)
@@ -25,22 +25,24 @@ async function compareImages(filename) {
 
         if (diffCount > 0) {
             await fs.promises.writeFile(`${diffDir}/${filename}`, PNG.sync.write(diff))
+            console.log(`##teamcity[publishArtifacts '${buffer}/${filename} => .teamcity/puppeteer/actual']`)
+            console.log(`##teamcity[testMetadata testName='${testName}' type='artifact' value='.teamcity/puppeteer/actual/${filename}' name='actual.image']`)
+            console.log(`##teamcity[publishArtifacts '${stable}/${filename} => .teamcity/puppeteer/expected']`)
+            console.log(`##teamcity[testMetadata testName='${testName}' type='artifact' value='.teamcity/puppeteer/expected/${filename}' name='expected.image']`)
             console.log(`##teamcity[publishArtifacts '${diffDir}/${filename} => .teamcity/puppeteer/diff']`)
-            console.log(`##teamcity[testMetadata testName='${testName}' type='artifact' value='.teamcity/puppeteer/diff/${filename}' name='teamcity.test.output']`)
-            console.log(`##teamcity[testFailed name='${testName}' message='ERROR']`)
-        } else {
-
+            console.log(`##teamcity[testMetadata testName='${testName}' type='artifact' value='.teamcity/puppeteer/diff/${filename}' name='diff.image']`)
+            console.log(`##teamcity[testFailed name='${testName}' message='ERROR' flowId='${flowId}']`)
         }
     } catch (error) {
         console.trace("Error: ", error)
-        console.log(`##teamcity[testFailed name='${testName}' message='Error: ${error.code}' details='${escapeTCMessage(error.toString())}']`)
+        console.log(`##teamcity[testFailed name='${testName}' message='Error: ${error.code}' details='${escapeTCMessage(error.toString())}' flowId='${flowId}']`)
 
         if (error.code === "ENOENT") {
             await fs.promises.writeFile(`${diffDir}/${filename}`, generatedFile)
         }
     } finally {
         let duration = Date.now() - startTime
-        console.log(`##teamcity[testFinished name='${testName}' duration='${duration}']`)
+        console.log(`##teamcity[testFinished name='${testName}' duration='${duration}' flowId='${flowId}']`)
     }
 }
 
@@ -56,10 +58,13 @@ function escapeTCMessage(str) {
 async function launchCompare() {
     const files = await readBufferedFiles()
 
+    const flowId = Date.now()
+    console.log(`##teamcity[testSuiteStarted name='compare images' flowId='${flowId}']`)
     for (const filename of files) {
-        await compareImages(filename)
+        await compareImages(filename, flowId)
         console.log("File: ", filename)
     }
+    console.log(`##teamcity[testSuiteFinished name='compare images' flowId='${flowId}']`)
 }
 launchCompare()
 
--- puppeteer/generate-screenshots.js
+++ puppeteer/generate-screenshots.js
@@ -6,7 +6,10 @@ const {buffer, diffDir} = require('./paths');
     await cleanDirectory(buffer)
     await cleanDirectory(diffDir)
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage', "--no-sandbox", "--disable-setuid-sandbox"]
+    });
     const page = await browser.newPage();
     await page.goto('http://frontend:8041');
     await page.screenshot({path: `buffer/overview.png`});
