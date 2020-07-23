09478dabd47e77b96a9c7df8efdb8f1460a60a4e
Denis Lesnik <denis.lesnik@jetbrains.com>
2020-07-23 11:23:40 +0200

    Added jest-puppeteer.config.js

--- puppeteer/jest.config.js
+++ puppeteer/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
     "preset": "jest-puppeteer",
     "reporters": ["default", "jest-teamcity"],
-    launch: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
 }
